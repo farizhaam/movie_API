1dbd977c3690ae48c56f480cf6793f5a34436e88
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-09-30 16:25:42 +0200

    Update models.js

--- models.js
+++ models.js
@@ -34,18 +34,21 @@ let directorSchema = mongoose.Schema({
 
 //creating userSchema
 let userSchema = mongoose.Schema({
+    Name: String,
     Username: {type: String, required: true},
     Password: {type: String, required: true},
-    Emai: {type: String, required: true},
+    Email: {type: String, required: true},
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
-
-
 let Movie = mongoose.model('Movie', movieSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 let User = mongoose.model('User', userSchema);
 
 //exports models
 module.exports.Movie = Movie;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
 module.exports.User = User;
