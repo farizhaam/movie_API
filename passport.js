2de3e456f22c629f0da37a4cc9ecf8ef9f07cdf0
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-01 21:55:43 +0200

    Create passport.js

---
+++ passport.js
@@ -0,0 +1,47 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+//LocalStrategy takes a username and password from the request body
+//and use Mongoose to check your database for a user with the same username
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+    }, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({Username: username}, (error, user) => {
+        if(error) {
+            console.log(error);
+                return callback(error);
+        }
+
+        if (!user) {
+            console.log('incorrect username');
+            //response when the username is cannot be found
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+
+//JWTStrategy akkiws to authenticare users based on the JWT submitted alongside their request
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),//bearer token
+    secretOrKey: 'your_jwt_secret'//signature verifies that the sender of JWT it is whoo say it is
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}));
+
