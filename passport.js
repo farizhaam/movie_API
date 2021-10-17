dc184524cd80ab52c87daada7dd9fbfac8f1f1ea
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-17 17:23:49 +0200

    changing validate to valid

--- passport.js
+++ passport.js
@@ -28,7 +28,7 @@ passport.use(new LocalStrategy({
         }
 
         //callback for incorrect password
-        if (!user.validatePassword(password)) {
+        if (!user.validPassword(password)) {
             console.log('incorrect password');
             return callback(null, false, {message: 'Incorrect password.'});
         }
